cd4f56b0c7d384d0f1be233d3b0d67bffe55d220
Berat Murathan Bayram <brtbyram.61@gmail.com>
2024-03-26 16:29:36 +0300

    Add categories and accommodations data file

---
+++ src/data/categories.js
@@ -0,0 +1,100 @@
+const categoriesData = [
+    {
+        name: "Küçük evler",
+        imageURL: "https://a0.muscache.com/pictures/3271df99-f071-4ecf-9128-eb2d2b1f50f0.jpg"
+    },
+    {
+        name: "Güzel manzaralar",
+        imageURL: "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg"
+    },
+    {
+        name: "Muhteşem havuzlar",
+        imageURL: "https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg"
+    },
+    {
+        name: "Kulübeler",
+        imageURL: "https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg"
+    },
+    {
+        name: "Sörf",
+        imageURL: "https://a0.muscache.com/pictures/957f8022-dfd7-426c-99fd-77ed792f6d7a.jpg"
+    },
+    {
+        name: "Malikâneler",
+        imageURL: "https://a0.muscache.com/pictures/78ba8486-6ba6-4a43-a56d-f556189193da.jpg"
+    },
+    {
+        name: "Odalar",
+        imageURL: "https://a0.muscache.com/pictures/7630c83f-96a8-4232-9a10-0398661e2e6f.jpg"
+    },
+    {
+        name: "Tekneler",
+        imageURL: "https://a0.muscache.com/pictures/687a8682-68b3-4f21-8d71-3c3aef6c1110.jpg"
+    },
+    {
+        name: "Oda-kahvaltılar",
+        imageURL: "https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg"
+    },
+    {
+        name: "Hanoklar",
+        imageURL: "https://a0.muscache.com/pictures/51f5cf64-5821-400c-8033-8a10c7787d69.jpg"
+    },
+    {
+        name: "Minsular",
+        imageURL: "https://a0.muscache.com/pictures/48b55f09-f51c-4ff5-b2c6-7f6bd4d1e049.jpg"
+    },
+    {
+        name: "Denize sıfır",
+        imageURL: "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg"
+    },
+    {
+        name: "Yurt çadırları",
+        imageURL: "https://a0.muscache.com/pictures/4759a0a7-96a8-4dcd-9490-ed785af6df14.jpg"
+    },
+    {
+        name: "Luxe",
+        imageURL: "https://a0.muscache.com/pictures/c8e2ed05-c666-47b6-99fc-4cb6edcde6b4.jpg"
+    },
+    {
+        name: "Kırsal",
+        imageURL: "https://a0.muscache.com/pictures/6ad4bd95-f086-437d-97e3-14d12155ddfe.jpg"
+    },
+    {
+        name: "Olağanüstü",
+        imageURL: "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg"
+    },
+    {
+        name: "Kamp",
+        imageURL: "https://a0.muscache.com/pictures/ca25c7f3-0d1f-432b-9efa-b9f5dc6d8770.jpg"
+    },
+    {
+        name: "Adalar",
+        imageURL: "https://a0.muscache.com/pictures/8e507f16-4943-4be9-b707-59bd38d56309.jpg"
+    },
+    {
+        name: "Üzüm bağları",
+        imageURL: "https://a0.muscache.com/pictures/60ff02ae-d4a2-4d18-a120-0dd274a95925.jpg"
+    },
+    {
+        name: "Dünyanın zirvesi",
+        imageURL: "https://a0.muscache.com/pictures/248f85bf-e35e-4dc3-a9a1-e1dbff9a3db4.jpg"
+    },
+    {
+        name: "Yeni",
+        imageURL: "https://a0.muscache.com/pictures/c0fa9598-4e37-40f3-b734-4bd0e2377add.jpg"
+    },
+    {
+        name: "Tarihî evler",
+        imageURL: "https://a0.muscache.com/pictures/33dd714a-7b4a-4654-aaf0-f58ea887a688.jpg"
+    },
+    {
+        name: "Tropik",
+        imageURL: "https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg"
+    },
+    {
+        name: "Şatolar",
+        imageURL: "https://a0.muscache.com/pictures/1b6a8b70-a3b6-48b5-88e1-2243d9172c06.jpg"
+    }
+];
+
+export default categoriesData;
