cb154e78b95292d16bb16847ae5ff5b43d32f89c
Berat Murathan Bayram <brtbyram.61@gmail.com>
2024-03-26 16:31:58 +0300

    Add responsive grid layout and category modal to home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import accommodations from "../../data/accommodations"
+import AccommodationCard from "../../components/cards/AccommodationCard"
+import { useMediaQuery } from 'react-responsive'
+import clsx from "clsx"
+import CategoryModal from "../../components/modals/CategoryModal"
 
 function Home() {
+
+  const isMobile = useMediaQuery({ query: '(min-width: 200px)' })
+  const isTablet = useMediaQuery({ query: '(min-width: 550px)' })
+  const isLaptop = useMediaQuery({ query: '(min-width: 950px)' })
+  const isDesktop = useMediaQuery({ query: '(min-width: 1130px)' })
+  const isBigScreen = useMediaQuery({ query: '(min-width: 1640px)' })
+
+
+
   return (
-    <div>Home</div>
+    <div>
+      <div className={clsx("grid gap-x-6 gap-y-10", {
+        "grid-cols-1": isMobile,
+        "grid-cols-2": isTablet,
+        "grid-cols-3": isLaptop,
+        "grid-cols-4": isDesktop,
+        "grid-cols-6": isBigScreen
+      })}>
+        {accommodations.map((accommodation, index) => (
+          <AccommodationCard key={index} accommodation={accommodation} />
+        ))}
+      </div>
+      <CategoryModal/>
+    </div>
   )
 }
 
