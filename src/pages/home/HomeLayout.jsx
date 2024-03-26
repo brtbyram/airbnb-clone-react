2f5592486769c79736acf9c2bd4dacc34e603104
Berat Murathan Bayram <brtbyram.61@gmail.com>
2024-03-26 16:30:40 +0300

    Add Categories component to HomeLayout

--- src/pages/home/HomeLayout.jsx
+++ src/pages/home/HomeLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../../components/header'
+import Categories from '../../components/categories'
 
 function HomeLayout() {
   return (
-    <div>
+    <div className='relative mx-6 md:mx-10 2xl:mx-20'>
       <Header />
+      <Categories/>
       <Outlet />
     </div>
   )
