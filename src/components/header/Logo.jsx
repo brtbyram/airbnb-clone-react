676f95bc3968ff1372429dfc90827ca7a71f0991
Berat Murathan Bayram <brtbyram.61@gmail.com>
2024-03-26 16:28:15 +0300

    Update Logo component in header

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom"
 
 function Logo() {
   return (
-    <div className='w-28 flex items-center'>
-      <Link to='/'>
-        <img src="https://links.papareact.com/qd3" alt="airbnb" />
+    <div className='min-w-28 pr-56 flex items-center'>
+      <Link to='/' className="flex items-center space-x-1 justify-center py-2">
+        <img src="https://seeklogo.com/images/A/airbnb-logo-1D03C48906-seeklogo.com.png" alt="airbnb" className="w-[30px]" />
+        <span className="text-[#ff385c] text-2xl font-bold tracking-tighter">airbnb</span>
       </Link>
     </div>
   )
